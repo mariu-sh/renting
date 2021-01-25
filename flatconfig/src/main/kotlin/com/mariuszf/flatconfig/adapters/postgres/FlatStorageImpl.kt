98e95e7d48ad42dd15074e29d49d85c276ff42b5
mariu-sh <figiel.mariusz1@gmail.com>
2021-01-25 23:34:47 +0100

    Splitting Storage implementation

--- flatconfig/src/main/kotlin/com/mariuszf/flatconfig/adapters/postgres/FlatStorageImpl.kt
+++ flatconfig/src/main/kotlin/com/mariuszf/flatconfig/adapters/postgres/FlatStorageImpl.kt
@@ -2,33 +2,43 @@ package com.mariuszf.flatconfig.adapters.postgres
 
 import com.mariuszf.flatconfig.application.port.out.FlatStorage
 import com.mariuszf.flatconfig.application.service.Flat
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Repository
 import java.util.*
-import javax.persistence.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
 
 @Component
-class FlatStorageImpl(
-    val postgresFlatStorage: PostgresFlatStorage
-) : FlatStorage {
+class FlatStorageImpl(val postgresFlatStorage: PostgresFlatStorage) : FlatStorage {
 
     override fun createFlat(totalSurface: Double): Flat =
         postgresFlatStorage.save(FlatEntity(UUID.randomUUID(), totalSurface)).toDomain()
 
-    override fun updateFlat(flatId: UUID, totalSurface: Double): Flat {
-        val flatEntity = postgresFlatStorage.findById(flatId).get()
-        flatEntity.totalSurface = totalSurface
-        return postgresFlatStorage.save(flatEntity).toDomain()
-    }
+    override fun updateFlat(flatId: UUID, totalSurface: Double): Flat =
+        try {
+            val flatEntity = postgresFlatStorage.findById(flatId).get()
+            flatEntity.totalSurface = totalSurface
+            postgresFlatStorage.save(flatEntity).toDomain()
+        } catch (_: EmptyResultDataAccessException) {
+            throw  FlatNotFoundInStorageException("Flat with id $flatId not found")
+        }
 
-    override fun findFlatById(flatId: UUID): Flat = postgresFlatStorage.findById(flatId)
-        .map { it.toDomain() }
-        .orElseThrow { Exception() }
+    override fun findFlatById(flatId: UUID): Flat =
+        postgresFlatStorage.findById(flatId).map { it.toDomain() }
+            .orElseThrow { FlatNotFoundInStorageException("Flat with id $flatId not found") }
 
     override fun findAllFlats(): List<Flat> = postgresFlatStorage.findAll().map { it.toDomain() }
 
-    override fun deleteFlatById(flatId: UUID) = postgresFlatStorage.deleteById(flatId)
+    override fun deleteFlatById(flatId: UUID) =
+        try {
+            postgresFlatStorage.deleteById(flatId)
+        } catch (_: EmptyResultDataAccessException) {
+            throw FlatNotFoundInStorageException("Flat with id $flatId not found")
+        }
 }
 
 @Repository
--- flatconfig/src/main/kotlin/com/mariuszf/flatconfig/adapters/postgres/RoomStorageImpl.kt
+++ flatconfig/src/main/kotlin/com/mariuszf/flatconfig/adapters/postgres/RoomStorageImpl.kt
@@ -2,41 +2,51 @@ package com.mariuszf.flatconfig.adapters.postgres
 
 import com.mariuszf.flatconfig.application.port.out.RoomStorage
 import com.mariuszf.flatconfig.application.service.Room
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Repository
 import java.util.*
-import javax.persistence.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
 
 @Component
 class RoomStorageImpl(
     val postgresRoomStorage: PostgresRoomStorage
-): RoomStorage {
+) : RoomStorage {
 
     override fun createRoom(surface: Double, flatId: UUID): Room =
         postgresRoomStorage.save(RoomEntity(UUID.randomUUID(), surface, flatId)).toDomain()
 
-    override fun findRoomById(roomId: UUID): Room = postgresRoomStorage.findById(roomId)
-        .map { it.toDomain() }
-        .orElseThrow { Exception() }
+    override fun findRoomById(roomId: UUID): Room =
+        postgresRoomStorage.findById(roomId).map { it.toDomain() }
+            .orElseThrow { RoomNotFoundInStorageException("Room with id $roomId not found") }
 
     override fun findAllRooms(): List<Room> = postgresRoomStorage.findAll().map { it.toDomain() }
 
-    override fun updateRoom(roomId: UUID, surface: Double): Room {
-        val roomEntity = postgresRoomStorage.findById(roomId).get()
-        roomEntity.surface = surface
-        return postgresRoomStorage.save(roomEntity).toDomain()
+    override fun updateRoom(roomId: UUID, surface: Double): Room =
+        try {
+            val roomEntity = postgresRoomStorage.findById(roomId).get()
+            roomEntity.surface = surface
+            postgresRoomStorage.save(roomEntity).toDomain()
+        } catch (_: EmptyResultDataAccessException) {
+            throw RoomNotFoundInStorageException("Room with id $roomId not found")
+        }
+
+    override fun deleteRoom(roomId: UUID) = try {
+        postgresRoomStorage.deleteById(roomId)
+    } catch (_: EmptyResultDataAccessException) {
+        throw RoomNotFoundInStorageException("Room with id $roomId not found")
     }
 
-    override fun deleteRoom(roomId: UUID) = postgresRoomStorage.deleteById(roomId)
-
     override fun findRoomsForFlatById(flatId: UUID): List<Room> =
-        postgresRoomStorage.findRoomEntitiesByFlatId(flatId)
-            .map { it.toDomain() }
+        postgresRoomStorage.findRoomEntitiesByFlatId(flatId).map { it.toDomain() }
 }
 
 @Repository
-interface PostgresRoomStorage: JpaRepository<RoomEntity, UUID>{
+interface PostgresRoomStorage : JpaRepository<RoomEntity, UUID> {
     fun findRoomEntitiesByFlatId(flatId: UUID): List<RoomEntity>
 }
 
@@ -47,7 +57,8 @@ data class RoomEntity(
     @Id @Column(name = "room_id") val id: UUID?,
     @Column(name = "surface") var surface: Double,
     @Column(name = "flat_id") val flatId: UUID?
-){
-    constructor(): this(null, 0.0, null)
+) {
+    constructor() : this(null, 0.0, null)
+
     fun toDomain(): Room = Room(id!!, surface, flatId!!)
 }
