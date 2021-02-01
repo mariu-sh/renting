e711090591920bf985cb7d08e525f05a9f67255f
mariu-sh <figiel.mariusz1@gmail.com>
2021-02-01 11:20:28 +0100

    initial for service

---
+++ costservice/src/test/kotlin/com/mariuszf/costservice/CostServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mariuszf.costservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CostServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
